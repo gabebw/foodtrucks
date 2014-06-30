b9d3802b99e958e5f0119ee44161e9493fa71f84
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-30 10:44:44 -0400

    Add menus and twitter names for most food trucks
    Baja Taco Truck and Chubby Chickpea don't have menus.
    
    Evan's NY Deli and Meng's Kitchen don''t have twitter accounts.
    
    It's pretty obvious what Chicken and Rice Guys sell.

--- lib/pretty_name_printer.rb
+++ lib/pretty_name_printer.rb
@@ -1,20 +1,33 @@
 class PrettyNamePrinter
   MENU_URLS = {
     "Bon Me" => "http://www.bonmetruck.com/menu/",
+    "Cookie Monstah" => "http://thecookiemonstah.com/ox_portfolio/portfolio/",
     "Evan's NY Style Deli" => "http://www.evansnewyorkstyledeli.com/our-food-truck/food-truck-menu/",
     "Mei Mei Street Kitchen" => "http://meimeiboston.tumblr.com/",
+    "Meng's Kitchen" => 'http://www.yelp.com/biz/mengs-kitchen-boston?hrid=KCvaa0cLk5nWAARnQUNdww',
     "Momogoose" => "http://sategrill.com/cafe-menu/",
+    "Moyzilla" => "http://boston.menupages.com/restaurants/moyzilla/menu",
+    "Paris Creperie" => "http://pariscreperie.com/la-tour-eiffel-bostons-very-first-crepe-truck/",
+    "Roxy's Gourmet Grilled Cheese" => 'http://www.roxysgrilledcheese.com/menu/',
+    "Stoked Wood Fired Pizza Co." => "http://www.stokedpizzaco.com/menu",
+    "Tenoch Mexican" => "http://www.tenochmexican.com/menu/",
+    "The Dining Car" => "http://diningcar.net/menu/",
   }
 
   TWITTER_NAMES = {
-    'Bon Me' => 'bonme',
-    'Chicken & Rice Guys' => 'CnRG_Boston',
-    'Cookie Monstah' => 'MonstahTruck',
-    'Mei Mei Street Kitchen' => 'meimeiboston',
-    'Momogoose' => 'momogoose',
-    'Moyzilla' => 'MoyzillaTruck',
-    "Roxy's Gourmet Grilled Cheese" => 'RoxysGrilledChz',
-    'Tenoch Mexican' => 'TenochMexican',
+    "Baja Taco Truck" => "BajaTacoTruck",
+    "Chubby Chickpea" => "chubbyfoodtruck",
+    "Bon Me" => "bonme",
+    "Chicken & Rice Guys" => "CnRG_Boston",
+    "Cookie Monstah" => "MonstahTruck",
+    "Mei Mei Street Kitchen" => "meimeiboston",
+    "Momogoose" => "momogoose",
+    "Moyzilla" => "MoyzillaTruck",
+    "Paris Creperie" => "ParisCreperie",
+    "Roxy's Gourmet Grilled Cheese" => "RoxysGrilledChz",
+    "Stoked Wood Fired Pizza Co." => "StokedPizza",
+    "Tenoch Mexican" => "TenochMexican",
+    "The Dining Car" => "thediningcar",
   }
 
   def initialize(name)
