17c0b36f2851a80c4f0eaeeaa60571dcceb78db8
Gabe Berke-Williams <gabebw@gabebw.com>
2014-06-30 20:52:09 -0400

    Caching is more trouble than it is worth
    Setting the `Cache-Control` header to `ONE_DAY` meant that it was being reset
    every time a user visited the site. The goal was really to have the
    `Cache-Control` set to "the end of today", rather than "one day from this
    second".
    
    That's easily implemented, but is honestly more trouble than it's worth, so I'm
    just removing caching.

--- app.rb
+++ app.rb
@@ -3,16 +3,8 @@ Bundler.require
 
 require 'open-uri'
 
-ONE_DAY = 86400
-
 Dir['./lib/*'].each do |f|
   require f
-end
-
-configure :production do
-  before do
-    cache_control :public, max_age: ONE_DAY
-  end
 end
 
 get '/' do
